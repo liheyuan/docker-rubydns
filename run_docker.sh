05d979c7980b804c618eb018ba107cd6e6fa888d
coder4 <lihy@fenbi.com>
2017-11-27 16:12:51 +0800

    FIX: Bug

--- run_docker.sh
+++ run_docker.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 HOSTS_PARAM=""
-if [ x"$HOSTS" != x"" ];
+if [ x"$HOSTS" != x"" ];then
     HOSTS_PARAM = " -t $HOSTS"
 fi
 
 UPSTREAM_PARAM=""
-if [ x"$UPSTREAM" != x"" ];
+if [ x"$UPSTREAM" != x"" ];then
     UPSTREAM_PARAM = " -s $UPSTREAM"
 fi
 
 REST_URL_PARAM=""
-if [ x"$REST_URL" != x"" ];
+if [ x"$REST_URL" != x"" ];then
     REST_URL_PARAM = " -u $REST_URL"
 fi
 
 IP_PREFIX_PARAM=""
-if [ x"$IP_PREFIX" != x"" ];
+if [ x"$IP_PREFIX" != x"" ];then
     IP_PREFIX_PARAM = " -p $IP_PREFIX"
 fi
 
