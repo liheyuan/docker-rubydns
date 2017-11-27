e5f79e19e21e8ade1e875247b065a4c67933546e
coder4 <lihy@fenbi.com>
2017-11-27 16:16:13 +0800

    FIX: Bug in shell

--- run_docker.sh
+++ run_docker.sh
@@ -2,22 +2,22 @@
 
 HOSTS_PARAM=""
 if [ x"$HOSTS" != x"" ];then
-    HOSTS_PARAM = " -t $HOSTS"
+    HOSTS_PARAM=" -t $HOSTS"
 fi
 
 UPSTREAM_PARAM=""
 if [ x"$UPSTREAM" != x"" ];then
-    UPSTREAM_PARAM = " -s $UPSTREAM"
+    UPSTREAM_PARAM=" -s $UPSTREAM"
 fi
 
 REST_URL_PARAM=""
 if [ x"$REST_URL" != x"" ];then
-    REST_URL_PARAM = " -u $REST_URL"
+    REST_URL_PARAM=" -u $REST_URL"
 fi
 
 IP_PREFIX_PARAM=""
 if [ x"$IP_PREFIX" != x"" ];then
-    IP_PREFIX_PARAM = " -p $IP_PREFIX"
+    IP_PREFIX_PARAM=" -p $IP_PREFIX"
 fi
 
 bundle exec ./rubydns.rb $HOSTS_PARAM $UPSTREAM_PARAM $REST_URL_PARAM $IP_PREFIX_PARAM
